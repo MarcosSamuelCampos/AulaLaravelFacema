6c2f1ed1cef3810f66dc5d3ed7d77606e3cabb5b
MarcosSamuelCampos <kampusam@gmail.com>
2026-05-16 11:38:51 -0300

    feat: criacao do controller, e o template postagem, rota

---
+++ app/Http/Controllers/PostagemCotroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostagemCotroller extends Controller
+{
+    public function index(){
+        return view('postagem');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostagemCotroller;
+use App\Models\postagem;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index',[PostagemCotroller::class,'index'])->name('index');
