74a4a21f167871ebefd6fcda3b4ab2c690bf4750
MarcosSamuelCampos <kampusam@gmail.com>
2026-05-16 11:17:19 -0300

    criação do arquivo:postagem,style

---
+++ resources/views/postagem.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+    |<header>
+        <h1>Meu blog simples</h1>
+    </header>
+    <footer>
+        <p>&copy; {{date('y')}} - Blog simples</p>
+    </footer>
+</body>
+</html>
